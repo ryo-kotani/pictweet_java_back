44ab926185608355e9d704a6313194ccd6165307
Ryo Katsuno <62652429+Katsuno0814@users.noreply.github.com>
2024-10-18 16:56:09 +0900

    ユーザーがログインの処理

--- src/test/java/in/tech_camp/pictweet/system/CommentIntegrationTest.java
+++ src/test/java/in/tech_camp/pictweet/system/CommentIntegrationTest.java
@@ -1,16 +1,33 @@
 package in.tech_camp.pictweet.system;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 import in.tech_camp.pictweet.PicTweetApplication;
+import in.tech_camp.pictweet.entity.CommentEntity;
+import in.tech_camp.pictweet.entity.TweetEntity;
 import in.tech_camp.pictweet.entity.UserEntity;
 import in.tech_camp.pictweet.factory.UserFormFactory;
 import in.tech_camp.pictweet.form.UserForm;
 import in.tech_camp.pictweet.service.UserService;
+import jakarta.servlet.http.HttpSession;
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = PicTweetApplication.class)
@@ -18,6 +35,9 @@ import in.tech_camp.pictweet.service.UserService;
 public class CommentIntegrationTest {
   private UserForm userForm;
   private UserEntity userEntity;
+
+  @Autowired
+  private MockMvc mockMvc;
 
   @Autowired
   private UserService userService;
@@ -33,4 +53,19 @@ public class CommentIntegrationTest {
 
       userService.registerNewUser(userEntity);
   }
+   @Test
+ public void ログインしたユーザーはツイート詳細ページでコメント投稿できる() throws Exception {
+     // ユーザーがログインする
+     MvcResult loginResult = mockMvc.perform(post("/login")
+     .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+     .param("username", userForm.getEmail())
+     .param("password", userForm.getPassword())
+     .with(csrf()))
+     .andExpect(status().isFound())
+     .andExpect(redirectedUrl("/"))
+     .andReturn();
+
+
+     HttpSession session = loginResult.getRequest().getSession();
+ }
 }
