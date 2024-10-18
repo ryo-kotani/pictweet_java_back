b3dc4c2c1569e835c7d7473a8098e63fa751bd90
Ryo Katsuno <62652429+Katsuno0814@users.noreply.github.com>
2024-10-18 16:52:34 +0900

    テスト用のユーザー情報をセットアップ

--- src/test/java/in/tech_camp/pictweet/system/CommentIntegrationTest.java
+++ src/test/java/in/tech_camp/pictweet/system/CommentIntegrationTest.java
@@ -1,6 +1,36 @@
 package in.tech_camp.pictweet.system;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import in.tech_camp.pictweet.PicTweetApplication;
+import in.tech_camp.pictweet.entity.UserEntity;
+import in.tech_camp.pictweet.factory.UserFormFactory;
+import in.tech_camp.pictweet.form.UserForm;
+import in.tech_camp.pictweet.service.UserService;
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = PicTweetApplication.class)
+@AutoConfigureMockMvc
 public class CommentIntegrationTest {
+  private UserForm userForm;
+  private UserEntity userEntity;
 
+  @Autowired
+  private UserService userService;
 
+  @BeforeEach
+  public void setup() {
+      // テスト用のユーザー情報をセットアップ
+      userForm = UserFormFactory.createUser();
+      userEntity = new UserEntity();
+      userEntity.setEmail(userForm.getEmail());
+      userEntity.setNickname(userForm.getNickname()); // 必要であれば他のフィールドもセット
+      userEntity.setPassword(userForm.getPassword());
+
+      userService.registerNewUser(userEntity);
+  }
 }
